8cf8d758f422763e944c18a8e9ca267691f2779f
kkv263 <kevinvu@wireless-10-145-151-206.public.utexas.edu>
2018-07-24 15:09:33 -0500

    add tab icons to home page

--- components/calc.js
+++ components/calc.js
@@ -1,16 +1,29 @@
 import React, { Component } from 'react';
-import {StyleSheet, Text, View } from 'react-native';
+import {StyleSheet, Text, View, Image } from 'react-native';
 
 export default class Calculator extends Component {
   render() {
     return (
       <View style={styles.container}>
-        <View style={{flex:1, flexDirection:'row', justifyContent:'center'}}>
-          <View style={styles.tablineActive}></View>
-          <View style={styles.tabline}></View>
-          <View style={styles.tabline}></View>
+        <View style={{flexDirection:'row', justifyContent:'space-around', marginTop: '3%', marginBottom: '2%'}}>
+          <Image style={{width:30, height:30, }} source={require('../assets/calcicon.png')}></Image>
+          <Image style={{width:30, height:30, }} source={require('../assets/circicon.png')}></Image>
+          <Image style={{width:23, height:30, }} source={require('../assets/noteicon.png')}></Image>
         </View>
-
+        <View style={{flexDirection:'row', justifyContent:'center'}}>
+          <View style={styles.tablineActive}>
+          </View>
+          <View style={styles.tabline}>
+          </View>
+          <View style={styles.tabline}>
+          </View>
+        </View>
+        <View style={styles.infoContainer}>
+          <View style={styles.infoCategory}></View>
+          <View style={styles.infoCategory}></View>
+          <View style={styles.infoCategory}></View>
+          <View style={styles.infoCategory}></View>
+        </View>
       </View>
     );
   }
@@ -20,7 +33,7 @@ const styles = StyleSheet.create({
     container: {
       width: '100%',
       height: '100%',
-      backgroundColor: '#33AFE9'
+      backgroundColor: '#33AFE9',
     },
     tabline: {
       width:'32%',
@@ -32,6 +45,20 @@ const styles = StyleSheet.create({
       width:'32%',
       height:5,
       backgroundColor: 'white',
+      opacity:0.5
+    },
+    infoCategory: {
+      flex:1,
+      flexGrow: 1,
+      flexBasis: '50%',
+      height: '50%',
+   },
+    infoContainer: {
+      flexDirection:'row',
+      flexWrap: 'wrap',
+      height: 130,
+      borderBottomWidth:1,
+      borderColor: 'white',
       opacity:0.5
     }
   });
